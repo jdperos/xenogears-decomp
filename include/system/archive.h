44be6dd8d74a107e478c8fe6c6383a3512096398
ladysilverberg <mystic.sheik@gmail.com>
2025-04-16 15:27:44 +0200

    Decompile ArchiveGetFilePath

--- include/system/archive.h
+++ include/system/archive.h
@@ -2,7 +2,9 @@
 #define _XENO_ARCHIVE_H
 
 #define ARCHIVE_HEADER_ENTRY_SIZE 0x7
+#define ARCHIVE_ENTRY_NAME_BUFFER_SIZE 0x40
 
+extern u32 D_8004FE48;
 extern u32 g_ArchiveTable;
 extern u32 g_CurArchiveOffset;
 
--- src/slus_006.64/system/archive.c
+++ src/slus_006.64/system/archive.c
@@ -27,13 +27,22 @@ INCLUDE_ASM("asm/slus_006.64/nonmatchings/system/archive", ArchiveDecodeAlignedS
 
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/system/archive", func_80028928);
 
-INCLUDE_ASM("asm/slus_006.64/nonmatchings/system/archive", func_80028998);
+char* ArchiveGetFilePath(unsigned int entryIndex) {
+    u32 nOffset;
+    
+    if (D_8004FE48) {
+        nOffset = (entryIndex + g_CurArchiveOffset - 1) * ARCHIVE_ENTRY_NAME_BUFFER_SIZE;
+        return nOffset + D_8004FE48;
+    }
+
+    return NULL;
+}
 
 int ArchiveDecodeSector(unsigned int entryIndex) {
     u8* pArchiveEntry;
-    u32 offset;
+    u32 nOffset;
 
-    offset = ((entryIndex + g_CurArchiveOffset - 1) * ARCHIVE_HEADER_ENTRY_SIZE);
-    pArchiveEntry = offset + g_ArchiveTable;
+    nOffset = (entryIndex + g_CurArchiveOffset - 1) * ARCHIVE_HEADER_ENTRY_SIZE;
+    pArchiveEntry = nOffset + g_ArchiveTable;
     return (pArchiveEntry[2] << 0x10) + (pArchiveEntry[1] << 0x8) + pArchiveEntry[0];
 }
