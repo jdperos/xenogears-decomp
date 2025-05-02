48cf79ff8f460968c2254b913c1ce825ed5dffb9
ladysilverberg <mystic.sheik@gmail.com>
2025-05-02 14:46:50 +0200

    libetc Split fixes

--- src/slus_006.64/psyq/libetc.c
+++ src/slus_006.64/psyq/libetc.c
@@ -77,9 +77,13 @@ INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", memclrIntr);
 
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", setjmp);
 
+INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", longjmp);
+
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", func_8004BE8C);
 
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", func_8004BED0);
+
+INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", func_8004BED8);
 
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libetc", ReturnFromException);
 
