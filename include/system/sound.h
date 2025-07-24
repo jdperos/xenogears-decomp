c7487d560fd6fa37d6b6685a9bc4826d2e8e1cde
jperos@jenova.games <jperos@jenova.games>
2025-07-24 03:40:50 -0400

    Found an array of VoiceData struct pointers in game data. Decompile SoundClearVoiceDataPointers

--- include/system/sound.h
+++ include/system/sound.h
@@ -3,6 +3,32 @@
 
 #include "psyq/libspu.h"
 
+#define NUM_VOICES 24
+
 extern SpuCommonAttr g_SoundSpuCommonAttr;
+
+typedef struct {
+    u8 m_Unk0[2];
+    u16 m_ModeFlags;
+    u8 m_Unk4[2];
+    u16 m_Flags;
+    s16 m_VolumeL;
+    s16 m_VolumeR;
+    u8 m_UnkC[8];
+    u16 m_Pitch;
+    u8 Unk16[6];
+    u32 m_StartAddress;
+    u32 m_LoopAddress;
+    u8 m_UnkAdsr1;
+    u8 m_UnkAdsr2;
+    u8 m_UnkAdsr3;
+    u8 m_UnkAdsr4;
+    u8 m_AdsrDR;
+    u8 m_UnkAdsr5;
+    u8 m_UnkAdsr6;
+    u8 m_AdsrSR;
+} SoundVoiceData;
+
+extern SoundVoiceData g_SoundVoiceDataPointerArray;
 
 #endif
