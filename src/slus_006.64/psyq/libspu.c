b116e87056a6af5221c99ee4495d47e86364d929
jperos@jenova.games <jperos@jenova.games>
2025-08-05 00:52:01 -0400

    Decompile _SpuIsInAllocateArea_

--- src/slus_006.64/psyq/libspu.c
+++ src/slus_006.64/psyq/libspu.c
@@ -606,9 +606,9 @@ long SpuSetReverb (long on_off) // 100% matching on PSYQ4.0 (gcc 2.7.2 + aspsx 2
 #define SPU_MALLOC_SKIP_ENTRY      (1 << 31)
 #define SPU_MALLOC_FLAGS           (SPU_MALLOC_UNK_FLAG_1 | SPU_MALLOC_UNK_FLAG_2 | SPU_MALLOC_END_MARKER | SPU_MALLOC_SKIP_ENTRY)
 #define SPU_MALLOC_ADDR_MASK       (~SPU_MALLOC_FLAGS)
-s32 _SpuIsInAllocateArea(u32 addr) {
+long _SpuIsInAllocateArea(u_long addr) {
     SPU_MALLOC* mem_entry;
-    int index;
+    long index;
     SPU_MALLOC* mem_list = (SPU_MALLOC*)_spu_memList;
 
     if (_spu_memList == NULL) {
@@ -634,7 +634,36 @@ s32 _SpuIsInAllocateArea(u32 addr) {
     return 0;
 }
 
-INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libspu", _SpuIsInAllocateArea_);
+// The same as _SpuIsInAllocateArea, but with an SPU-ranged memory address
+long _SpuIsInAllocateArea_(long addr) {
+    SPU_MALLOC* mem_entry;
+    long index;
+    SPU_MALLOC* mem_list = (SPU_MALLOC*)_spu_memList;
+
+    addr <<= _spu_mem_mode_plus;
+
+    if (_spu_memList == NULL) {
+        return 0;
+    }
+
+    for (index = 0; 1; index++) {
+        mem_entry = &mem_list[index];
+
+        if (mem_entry->addr & SPU_MALLOC_SKIP_ENTRY) continue;
+
+        if (mem_entry->addr & SPU_MALLOC_END_MARKER) break;
+
+        if ((mem_entry->addr & SPU_MALLOC_ADDR_MASK) >= addr) {
+            return 1;
+        }
+
+        if (((mem_entry->addr & SPU_MALLOC_ADDR_MASK) + mem_entry->size) > addr) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 // TODO(jperos): Replace these constants as soon as the docs make sense
 long SpuReadDecodedData(SpuDecodedData *d_data, long flag) {
