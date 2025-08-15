c844bbe34fc73e377b2f92b9c16f0cea39aea6bd
Jonathan Peros <jperos@jenova.games>
2025-08-15 11:54:30 -0400

    Decompile SpuReadDecodedData

--- src/slus_006.64/psyq/libspu/SpuReadDecodedData.c
+++ src/slus_006.64/psyq/libspu/SpuReadDecodedData.c
@@ -2,4 +2,23 @@
 
 #include "libspu_internal.h"
 
-INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libspu/SpuReadDecodedData", SpuReadDecodedData);
+// TODO(jperos): Replace these constants as soon as the docs make sense
+long SpuReadDecodedData(SpuDecodedData *d_data, long flag) {
+    switch (flag) {
+        case SPU_CDONLY:
+            _spu_Fr_(d_data->cd_left, 0, 0x20);
+            break;
+        case SPU_VOICEONLY:
+            _spu_Fr_(d_data->voice1, 0x100, 0x20);
+            break;
+        default:
+            _spu_Fr_(d_data->cd_left, 0, 0x40);
+            break;
+    }
+
+    if (_spu_RXX->rxx.spustat & SPU_STAT_CAPTURE_SECONDHALF) {
+        return SPU_DECODE_SECONDHALF;
+    } else {
+        return SPU_DECODE_FIRSTHALF;
+    }
+}
